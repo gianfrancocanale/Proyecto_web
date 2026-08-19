942f05f396a172afa7d626ab9fb5b25edc63f427
gianfrancocanale <gianfrancocanale159@gmail.com>
2026-08-19 16:34:27 -0300

    Entrega tp1

---
+++ README.md
@@ -0,0 +1,6 @@
+# TP1
+
+## Cómo ejecutar
+
+Abrir el archivo `index.html` en un navegador web.
+
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// 1. Define el directorio que contiene los archivos estáticos.
+	staticDir := "./static"
+
+	// 2. Crea un manejador de archivos estáticos usando http.FileServer.
+
+	fileServer := http.FileServer(http.Dir(staticDir))
+	// el FileServer maneja el Content-type automáticamente según la extensión del archivo.
+
+	// 3. Registra el manejador para que atienda todas las peticiones ("/").
+
+	http.Handle("/", fileServer)
+
+	// 4. Define el puerto y muestra un mensaje.
+	port := ":8080"
+	fmt.Printf("Servidor ESTÁTICO escuchando en http://localhost%s\n", port)
+	fmt.Printf("Sirviendo archivos desde: %s\n", staticDir)
+
+	// 5. Inicia el servidor.
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	}
+}
