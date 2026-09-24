dbd19287d64f1ab59f1b7aaf8e08e1dab9116e87
gianfrancocanale <gianfrancocanale159@gmail.com>
2026-09-24 10:37:07 -0300

    entidad gran premio en proceso 3/5

---
+++ handlers/granpremio.go
@@ -0,0 +1,131 @@
+package handlers
+
+import (
+	sqlc "Proyecto_web/db/sqlc"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+func CrearGranPremio(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var granPremio sqlc.CrearGranPremioParams
+	var piloto sqlc.ObtenerPilotoParams
+
+	err := json.NewDecoder(r.Body).Decode(&granPremio)
+	if err != nil {
+		http.Error(w, "Error al decodificar el cuerpo de la solicitud", http.StatusBadRequest)
+		return
+	}
+
+	if granPremio.IDGranPremio == "" || granPremio.Pais == "" || granPremio.CantidadVueltas == 0 || granPremio.CantidadVueltas < 0 || granPremio.IDUltimoGanador == 0 {
+		http.Error(w, "Todos los campos son obligatorios", http.StatusBadRequest)
+		return
+	}
+
+	queries := sqlc.New(db)
+
+	piloto, err := queries.ObtenerPiloto(r.Context(), granPremio.IDUltimoGanador)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.Error(w, "El último ganador no existe", http.StatusBadRequest)
+		} else {
+			http.Error(w, "Error al verificar el último ganador", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	devolver, err := queries.CrearGranPremio(r.Context(), granPremio)
+	if err != nil {
+		http.Error(w, "Error al crear el Gran Premio", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(devolver)
+}
+
+func ListarGranPremios(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	queries := sqlc.New(db)
+
+	granPremios, err := queries.ListarGranPremios(r.Context())
+	if err != nil {
+		http.Error(w, "Error al obtener los Gran Premios", http.StatusInternalServerError)
+		return
+	}
+	if len(granPremios) == 0 {
+		http.Error(w, "No se encontraron Gran Premios", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(granPremios)
+}
+
+func ObtenerGranPremio(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	idGranPremio := r.PathValue("id")
+
+	queries := sqlc.New(db)
+
+	granPremio, err := queries.ObtenerGranPremio(r.Context(), idGranPremio)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.Error(w, "Gran Premio no encontrado", http.StatusNotFound)
+		} else {
+			http.Error(w, "Error al obtener el Gran Premio", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(granPremio)
+}
+
+func ActualizarGranPremio(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	idGranPremio := r.PathValue("id")
+
+	var granPremio sqlc.ActualizarGranPremioParams
+	err := json.NewDecoder(r.Body).Decode(&granPremio)
+	if err != nil {
+		http.Error(w, "Error al decodificar el cuerpo de la solicitud", http.StatusBadRequest)
+		return
+	}
+
+	if granPremio.Pais == "" || granPremio.LongitudKm == 0 || granPremio.CantidadVueltas < 0 || granPremio.IDUltimoGanador == 0 {
+		http.Error(w, "Todos los campos son obligatorios", http.StatusBadRequest)
+		return
+	}
+
+	granPremio.IDGranPremio = idGranPremio
+
+	queries := sqlc.New(db)
+
+	_, err = queries.ActualizarGranPremio(r.Context(), granPremio)
+	if err != nil {
+		http.Error(w, "Error al actualizar el Gran Premio", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(granPremio)
+}
--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"fmt"
 	"net/http"
 	"os" //obtener datos de variables de entorno(bases de datos)
+
 	//convertir string a int
 
 	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 func main() {
 	db := conectarBase()
@@ -17,84 +20,150 @@ func main() {
 	defer db.Close()
 
 	// MANEJO DE RUTAS DE ENTIDAD USUARIO:
-	http.HandleFunc("/usuarios", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			h.CrearUsuario(db, w, r)
-		case http.MethodGet:
-			h.ListarUsuarios(db, w, r)
-		default:
-			http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-			fmt.Println("entro al default")
-		}
-	})
-
-	http.HandleFunc("/usuarios/{id}", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			h.ObtenerUsuario(db, w, r)
-		case http.MethodPut:
-			h.ActualizarUsuario(db, w, r)
-		case http.MethodDelete:
-			h.EliminarUsuario(db, w, r)
-		default:
-			http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-		}
-
-	})
+	http.HandleFunc("/usuarios", handlerUsuario)
+	http.HandleFunc("/usuarios/{id}", handlerUsuarioId)
 
 	//MANEJO DE RUTAS DE ENTIDAD PILOTO HISTORICO:
-	http.HandleFunc("/pilotos_historicos", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			h.CrearPilotoHistorico(db, w, r)
-		case http.MethodGet:
-			h.ListarPilotosHistoricos(db, w, r)
-		default:
-			http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-		}
-	})
-	http.HandleFunc("/pilotos_historicos/{id}", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			h.ObtenerPilotoHistorico(db, w, r)
-		case http.MethodPut:
-			h.ActualizarPilotoHistorico(db, w, r)
-		case http.MethodDelete:
-			h.EliminarPilotoHistorico(db, w, r)
-		default:
-			http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-		}
-	})
+	http.HandleFunc("/pilotos_historicos", handlerPilotoHistorico)
+	http.HandleFunc("/pilotos_historicos/{id}", handlerPilotoHistoricoId)
 
 	//MANEJO DE RUTAS DE ENTIDAD ESCUDERIA:
-	http.HandleFunc("/escuderias", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			h.CrearEscuderia(db, w, r)
-		case http.MethodGet:
-			h.ListarEscuderias(db, w, r)
-		default:
-			http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-		}
-	})
+	http.HandleFunc("/escuderias", handlerEscuderia)
+	http.HandleFunc("/escuderias/{id}", handlerEscuderiaId)
 
-	http.HandleFunc("/escuderias/{id}", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			h.ObtenerEscuderia(db, w, r)
-		case http.MethodPut:
-			h.ActualizarEscuderia(db, w, r)
-		case http.MethodDelete:
-			h.EliminarEscuderia(db, w, r)
-		default:
-			http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
-		}
-	})
+	//MANEJO DE RUTAS DE ENTIDAD PILOTO:
+	http.HandleFunc("/pilotos", handlerPiloto)
+	http.HandleFunc("/pilotos/{id}", handlerPilotoId)
+
+	//MANEJO DE RUTAS DE ENTIDAD GRAN PREMIO:
+	http.HandleFunc("/gran_premios", h.HandlerGranPremio)
+	http.HandleFunc("/gran_premios/{id}", h.HandlerGranPremioId)
 
 	fmt.Println(" ")
 	fmt.Println("Servidor escuchando en :8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func handlerGranPremio(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.CrearGranPremio(db, w, r)
+	case http.MethodGet:
+		h.ListarGranPremios(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlerGranPremioId(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.ObtenerGranPremio(db, w, r)
+	case http.MethodPut:
+		h.ActualizarGranPremio(db, w, r)
+	case http.MethodDelete:
+		h.EliminarGranPremio(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlerPiloto(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.CrearPiloto(db, w, r)
+	case http.MethodGet:
+		h.ListarPilotos(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlerPilotoId(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.ObtenerPiloto(db, w, r)
+	case http.MethodPut:
+		h.ActualizarPiloto(db, w, r)
+	case http.MethodDelete:
+		h.EliminarPiloto(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlerUsuario(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.CrearUsuario(db, w, r)
+	case http.MethodGet:
+		h.ListarUsuarios(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		fmt.Println("entro al default")
+	}
+}
+
+func handlerUsuarioId(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.ObtenerUsuario(db, w, r)
+	case http.MethodPut:
+		h.ActualizarUsuario(db, w, r)
+	case http.MethodDelete:
+		h.EliminarUsuario(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func handlerPilotoHistorico(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.CrearPilotoHistorico(db, w, r)
+	case http.MethodGet:
+		h.ListarPilotosHistoricos(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlerPilotoHistoricoId(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.ObtenerPilotoHistorico(db, w, r)
+	case http.MethodPut:
+		h.ActualizarPilotoHistorico(db, w, r)
+	case http.MethodDelete:
+		h.EliminarPilotoHistorico(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlerEscuderia(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		h.CrearEscuderia(db, w, r)
+	case http.MethodGet:
+		h.ListarEscuderias(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlerEscuderiaId(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.ObtenerEscuderia(db, w, r)
+	case http.MethodPut:
+		h.ActualizarEscuderia(db, w, r)
+	case http.MethodDelete:
+		h.EliminarEscuderia(db, w, r)
+	default:
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+	}
 }
 
 func conectarBase() *sql.DB {
